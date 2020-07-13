ce842c7c0c0b0599f4036cc372f88dedabb579a4
Nocholas_violet <Nocholas_violet@163.com>
2020-07-13 12:49:08 +0800

    02.(3.4.5) 日活跃统计,日下单,月增加用户日分类

--- meiduo_mall/meiduo_mall/apps/meiduo_admin/urls.py
+++ meiduo_mall/meiduo_mall/apps/meiduo_admin/urls.py
@@ -9,4 +9,10 @@ urlpatterns = [
     re_path(r'^statistical/total_count/$', UserTotalCountView.as_view()),
     # 用户日新增
     re_path(r'^statistical/day_increment/$', UserDayCountView.as_view()),
+    # 用户日活跃数量
+    re_path(r'^statistical/day_active/$', UserActiveCountView.as_view()),
+    # 日用户下订单
+    re_path(r'^statistical/day_orders/$', UserOrderCountView.as_view()),
+    # 日分类,月用户增加统计
+    re_path(r'^statistical/month_increment/$', UserMonthCountView.as_view()),
 ]
--- meiduo_mall/meiduo_mall/apps/meiduo_admin/views/home_views.py
+++ meiduo_mall/meiduo_mall/apps/meiduo_admin/views/home_views.py
@@ -37,5 +37,50 @@ class UserDayCountView(APIView):
         })
 
 
+# 日活跃用户统计
+class UserActiveCountView(APIView):
 
+    def get(self, request):
+        local_0_time = get_local_0_time()
+        count = User.objects.filter(last_login__gte=local_0_time).count()
+        return Response({
+            'count':count,
+            'date':local_0_time.date()
+        })
+
+
+# 日下订单用户统计
+class UserOrderCountView(APIView):
+
+    def get(self, request):
+        local_0_time = get_local_0_time()
+        count = User.objects.filter(orders__create_time__gte=local_0_time).count()
+        return Response({
+            'count':count,
+            'date':local_0_time.date()
+        })
+
+
+# 日分类商品访问,月增用户统计
+class UserMonthCountView(APIView):
+
+    def get(self, request):
+        # 获取当日零时刻,以及29天前的;零时刻
+        # 获取当日时间
+        local_0_time = get_local_0_time()
+        # 获取到29天前的日期
+        cur_0_time = local_0_time - timezone.timedelta(days=29)
+        user_count_list = []
+        # for循环遍历
+        for index in range(30):
+            start_time = cur_0_time + timezone.timedelta(days=index)
+            end_time = start_time + timezone.timedelta(days=1)
+
+            count = User.objects.filter(date_joined__gte=start_time, date_joined__lt=end_time).count()
+            user_count_list.append({
+                'count':count,
+                'date':cur_0_time.date()
+            })
+
+        return Response(user_count_list)
 
